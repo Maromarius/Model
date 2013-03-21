10d5b66a656c834fde26a5d8d80197c7479cc396
Rami Kuret <ramikuret@gmail.com>
2013-03-21 16:27:07 -0400

    First commit

---
+++ WHM/Appointments/IndividualAppointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHM
+{
+    class IndividualAppointment : Appointment
+    {
+        public Dependant Dependant { get; set; }
+
+        public IndividualAppointment(Dependant dep, int id, bool attended)
+            : base(id, attended)
+        {
+            Dependant = dep;
+        }
+    }
+}
---
+++ WHM/Dependant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WHM
+{
+    class Dependant
+    {
+        #region Fields
+
+        public int UniqueID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Medicare { get; set; }
+        public char Sexe { get; set; }
+        public List<int> Appointments { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Dependant(int id, string lastname, string firstname, int medicare, char sexe)
+        {
+            UniqueID = id;
+            LastName = lastname;
+            FirstName = firstname;
+            Medicare = medicare;
+            Sexe = sexe;
+        }
+
+
+        #endregion
+    }
+}
